8bdffe51179c8867f6edec33948ee78ab2ececef
hlolli <hlolli@gmail.com>
2017-07-31 21:45:42 +0200

    gui scrollbar

--- gui/front-end-script.js
+++ gui/front-end-script.js
@@ -1,23 +1,23 @@
 var ctrlDown = false;
 
 window.onkeyup = function(evt) {
-  var e = evt || window.event;
-  var code = e.keyCode ? e.keyCode : e.charCode;
-  if (code === 17) {
-    ctrlDown = false;
-  }
-}
+    var e = evt || window.event;
+    var code = e.keyCode ? e.keyCode : e.charCode;
+    if (code === 17) {
+	ctrlDown = false;
+    }
+};
 
 window.onkeydown = function(evt) {
-  var e = evt || window.event;
-  var code = e.keyCode ? e.keyCode : e.charCode;
-  if (code === 17) {
-    ctrlDown = true;
-  }
+    var e = evt || window.event;
+    var code = e.keyCode ? e.keyCode : e.charCode;
+    if (code === 17) {
+	ctrlDown = true;
+    }
 
-  if (ctrlDown && code === 13) {
-    evaluateExpression();
-  }
+    if (ctrlDown && code === 13) {
+	evaluateExpression();
+    }
 }
 
 
@@ -25,72 +25,72 @@ window.onkeydown = function(evt) {
 
 var baseLogFunction = console.log;
 console.log = function(){
-  baseLogFunction.apply(console, arguments);
-  var printArea = document.querySelector("#console");
-  var args = Array.prototype.slice.call(arguments);
-  for(var i=0;i<args.length;i++){
-    
-    if (!/=>/g.test(args[i].match)){
-      console.info(args[i], /=>/g.test(args[i].match));
-      printArea.innerHTML += args[i]
-    };
-    
-    printArea.scrollTop = printArea.scrollHeight - printArea.clientHeight;
-  }
+    baseLogFunction.apply(console, arguments);
+    var printArea = document.querySelector("#console");
+    var args = Array.prototype.slice.call(arguments);
+    for(var i=0;i<args.length;i++){
+	
+	if (!/=>/g.test(args[i].match)){
+	    console.info(args[i], /=>/g.test(args[i].match));
+	    printArea.innerHTML += args[i];
+	};
+	
+	printArea.scrollTop = printArea.scrollHeight - printArea.clientHeight;
+    }
 
 }
 
 function createLogNode(message){
-  var node = document.createElement("div");
-  var textNode = document.createTextNode(message);
-  node.appendChild(textNode);
-  return node;
+    var node = document.createElement("div");
+    var textNode = document.createTextNode(message);
+    node.appendChild(textNode);
+    return node;
 }
 
 window.onerror = function(message, url, linenumber) {
-  console.log("JavaScript error: " + message + " on line " +
-              linenumber + " for " + url);
+    console.log("JavaScript error: " + message + " on line " +
+		linenumber + " for " + url);
 }
 
 
 function evaluateExpression()
 {
-  var selectedText = editor.getSession().doc.getTextRange(editor.selection.getRange());
-  
-  var sexpIndx = sexpAtPoint(editor.getValue(),
-			     editor.session.doc.positionToIndex(editor.getCursorPosition()));
+    var selectedText = editor.getSession().doc.getTextRange(editor.selection.getRange());
+    
+    var sexpIndx = sexpAtPoint(editor.getValue(),
+			       editor.session.doc.positionToIndex(editor.getCursorPosition()));
 
-  if (selectedText.length === 0) {
-    if (sexpIndx) {
-      var start = editor.session.doc.indexToPosition(sexpIndx[0]);
-      var end = editor.session.doc.indexToPosition(sexpIndx[1]);
+    if (selectedText.length === 0) {
+	if (sexpIndx) {
+	    var start = editor.session.doc.indexToPosition(sexpIndx[0]);
+	    var end = editor.session.doc.indexToPosition(sexpIndx[1]);
 
-      // blink Expression
-      var _range = new Range(start['row'], 0, end['row'], 1);
-      _range.id = editor.session.addMarker(_range, "flashEval", "fullLine");
-      setTimeout(function(){
-	editor.session.removeMarker(_range.id);
-      }, 90);
-      
-      // send expression to lumo
-      var sexp = editor.getSession().doc.getTextRange(
-	{start: start,
-	 end: end});
-      ipcRenderer.send('cljs-command', sexp);
-      // console.log(sexp, "SEXP");
+	    // blink Expression
+	    var _range = new Range(start['row'], 0, end['row'], 1);
+	    _range.id = editor.session.addMarker(_range, "flashEval", "fullLine");
+	    setTimeout(function(){
+		editor.session.removeMarker(_range.id);
+	    }, 90);
+	    
+	    // send expression to lumo
+	    var sexp = editor.getSession().doc.getTextRange(
+		{start: start,
+		 end: end});
+	    ipcRenderer.send('cljs-command', sexp);
+	    // console.log(sexp, "SEXP");
+	}
+    } else {
+	ipcRenderer.send('cljs-command', selectedText);
     }
-  } else {
-    ipcRenderer.send('cljs-command', selectedText);
-  }
-  // console.log(sexp, selectedText);
+    // console.log(sexp, selectedText);
 }
 
-const {ipcRenderer} = require('electron')
+const {ipcRenderer} = require('electron');
 
 
-ipcRenderer.on('cljs-command', (event, arg) => {
-  console.log(arg) // prints "pong"
-})
+ipcRenderer.on('log', (event, arg) => {
+    console.log(arg); // prints "pong"
+});
 
 
 // ipcRenderer.send('console-log-ping');
--- gui/index.js
+++ gui/index.js
@@ -1,102 +1,114 @@
-const {app, BrowserWindow, ipcMain, ipcRenderer} = require('electron')
-const path = require('path')
-const url = require('url')
-const spawn = require('child_process').spawn
-const net = require('net')
+const {app, BrowserWindow, ipcMain, ipcRenderer} = require('electron');
+const path = require('path');
+const url = require('url');
+const spawn = require('child_process').spawn;
+const net = require('net');
 
 const lumoConnection = new net.Socket();
 // Hacky temp solution
 var logBuffer = "";
 
-let win
+let win;
 
 var proc = spawn('./node_modules/lumo-cljs/bin/lumo',
-		 ['-c', 'src:lib/andare-0.7.0.jar:lib/fs-0.1.0.jar', '-n', '5555', '-k', '.lumo_cache']);
+		 ['-c', 'src:lib/andare-0.7.0.jar:lib/fs-0.1.0.jar:lib/instaparse-cljs-1.4.1.2.jar', '-n', '5555', '-k', '.lumo_cache']);
 
 // proc.stdout.setEncoding('utf8');
+
+
+function getWindow(windowName) {
+    for (var i = 0; i < windowArray.length; i++) {
+	if (windowArray[i].name == windowName) {
+	    return windowArray[i].window;
+	}
+    }
+    return null;
+}
 
 function createWindow () {
   // Create the browser window.
   
-  win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({width: 800, height: 600});
 
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-  
-  // Open the DevTools.
-  // win.webContents.openDevTools()
-  
+    win.loadURL(url.format({
+	pathname: path.join(__dirname, 'index.html'),
+	protocol: 'file:',
+	slashes: true
+    }));
+    
+    // Open the DevTools.
+    // win.webContents.openDevTools()
+    
 
-  // proc.stdout.on('data', function (data) {
-  //   var str = data.toString()
-  //   var lines = str.split(/(\r?\n)/g);
-  //   console.log(lines.join("").toString('utf8'));
-  // });
+    // proc.stdout.on('data', function (data) {
+    //   var str = data.toString()
+    //   var lines = str.split(/(\r?\n)/g);
+    //   console.log(lines.join("").toString('utf8'));
+    // });
 
-  var triggerPanaeolus = function () {
-    lumoConnection.write(`(require 'panaeolus.all) \n`);
-  }
-  
-  setTimeout(() => {
-    lumoConnection.connect(5555, 'localhost', function() {
-      triggerPanaeolus();
-      console.log('Starting panaeolus, may take a while....');
-    })}, 500); 
+    var triggerPanaeolus = function () {
+	lumoConnection.write(`(require 'panaeolus.all) \n`);
+    };
+    
+    setTimeout(() => {
+	lumoConnection.connect(5555, 'localhost', function() {
+	    triggerPanaeolus();
+	    console.log('Starting panaeolus, may take a while....');
+	});
+    }, 500); 
 
-  lumoConnection.on('data', function(data) {
-    var logStrCand = data.toString('utf8'); 
-    if (!/=>/g.test(logStrCand))
-    {
-      logBuffer += `<div>${logStrCand}</div>`;
-    }
-    console.log(data.toString('utf8'));
-  });
+    lumoConnection.on('data', function(data) {
+	var logStrCand = data.toString('utf8'); 
+	if (!/=>/g.test(logStrCand))
+	{
+	    win.webContents.send('log', `<div>${logStrCand}</div>`);
+	    // logBuffer += `<div>${logStrCand}</div>`;
+	}
+	// console.log(data.toString('utf8'));
+    });
 
-  lumoConnection.on('close', function() {
-    console.log('Connection closed');
-  });
-  
+    lumoConnection.on('close', function() {
+	console.log('Connection closed');
+    });
+    
 
-  win.on('closed', () => {
-    win = null
-    lumoConnection.destroy();
-    proc.kill('SIGINT');
-  })
+    win.on('closed', () => {
+	win = null;
+	lumoConnection.destroy();
+	proc.kill('SIGINT');
+    });
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
   lumoConnection.destroy();
   proc.kill('SIGINT');
   if (process.platform !== 'darwin') {
-    app.quit()
+      app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+      createWindow();
   }
-})
+});
 
 
 
 ipcMain.on('cljs-command', (event, arg) => {
   lumoConnection.write(`${arg}\n`);  
   // event.sender.send('cljs-command', arg)
-  setTimeout(() => {
-    if (logBuffer.length) {
-      event.sender.send('cljs-command', logBuffer);
-      logBuffer = '';
-    }
-  }, 50)
-})
+    // setTimeout(() => {
+    //   if (logBuffer.length) {
+    //     event.sender.send('cljs-command', logBuffer);
+    //     logBuffer = '';
+    //   }
+    // }, 50);
+});
 
 // ipcMain.on('console-log-ping', (event, arg) => {
 //   setInterval(() => {
